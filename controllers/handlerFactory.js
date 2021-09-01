93262c1f4e0417dbea2ee4649d2302efadfa8ab5
leftyxiv <manny@aledoux.net>
2021-09-01 15:11:43 -0400

    added delete and update to the hander factory

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,37 @@
+const AppError = require('../utils/AppError');
+const tryCatchAsync = require('../utils/tryCatchAsync');
+const APIFeatures = require('../utils/ApiFeatures');
+
+exports.deleteOne = Model =>
+    tryCatchAsync(async (req, res, next) => {
+        const doc = await Model.findByIdAndDelete(req.params.id);
+
+        if (!doc) {
+            return next(new AppError('No document found with that ID', 404));
+        }
+
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    });
+
+exports.updateOne = Model =>
+    tryCatchAsync(async (req, res, next) => {
+        const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!doc) {
+            return next(new AppError('No document found with that ID', 404));
+        }
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                data: doc
+            }
+        });
+    });
+
