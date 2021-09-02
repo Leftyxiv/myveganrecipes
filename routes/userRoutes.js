611da3481dabc1691bdb1c239ed3302c88e579a8
leftyxiv <manny@aledoux.net>
2021-09-02 11:07:56 -0400

    added a user router

---
+++ routes/userRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/logout', authController.logout);
+
+router.post('/forgotpassword', authController.forgotPassword);
+router.patch('/resetpassword/:token', authController.resetPassword);
+
+router.use(authController.protect);
+
+router.patch('/updatemypassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateme', userController.uploadUserPhoto, userController.resizeUserPhoto, userController.updateMe);
+router.delete('/deleteme', userController.deleteMe);
+
+// router.use(authController.restrictTo('admin'));
+
+router.route('/').get(userController.getAllUsers)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
+
+module.exports = router;
