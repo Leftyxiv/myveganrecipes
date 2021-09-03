99cbc38aedc645fc393074d1d9899bc1f1c23f97
leftyxiv <manny@aledoux.net>
2021-09-03 09:58:32 -0400

    added recipe routes

---
+++ routes/recipeRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const authController = require('../controllers/authController');
+const recipeController = require('../controllers/recipeController');
+
+const router = express.Router({ mergeParams: true });
+
+// router.use(authController.protect);
+
+router.route('/')
+  .get(recipeController.getAllRecipes)
+  .post(
+    recipeController.setRecipUserIds,
+    recipeController.createRecipe
+  );
+
+router.route('/:id')
+  .get(recipeController.getRecipe)
+  .patch(
+      recipeController.setRecipUserIds,
+      recipeController.updateRecipe
+  )
+  .delete(recipeController.deleteRecipe);
+
+module.exports = router;
